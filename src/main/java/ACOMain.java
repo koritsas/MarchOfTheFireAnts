cfc9dcac54f6643049937ee3a1a03dae661bec4d
koritsas <il_koritsas@hotmail.co.uk>
2017-01-04 15:31:42 +0200

    fix

--- src/main/java/ACOMain.java
+++ src/main/java/ACOMain.java
@@ -185,7 +185,7 @@ public class ACOMain {
 
         environment.initilizePheromones(5);
        AntSystemAlogrithm AS = new AntSystemAlogrithm(environment,colony,100,0.5,0.2,0.8);
-        MaxMinAntSystemAlgorithm AS = new MaxMinAntSystemAlgorithm(environment,colony,500,0.000001,0.2,1,1,0,5);
+        MaxMinAntSystemAlgorithm AS = new MaxMinAntSystemAlgorithm(environment,colony,500,0.000001,0.2,1,1,5);
         AS.execute();
         AS.createCostGraph();
         GraphUtils.visualizeGraph(AS.getBestSolution().getGraph());
